1b668bba6507a158f06c28ef88a8fa44c544b0a0
Addison Guskey <addisonguskey@Addisons-MacBook-Pro.local>
2022-06-13 19:38:34 -0600

    models and routes updated

--- models/index.js
+++ models/index.js
@@ -1,16 +1,33 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Products.belongsTo(Category, {
+  foreign_key: "category_id",
+});
 
 // Categories have many Products
-
+Category.hasMany(Products, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tags, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+});
 
 module.exports = {
   Product,
